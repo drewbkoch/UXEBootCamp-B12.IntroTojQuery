90e9e7231c1474bfea6f7a2fb9db9567b1555976
Drew Koch <dkoch13@gmail.com>
2014-03-27 10:40:35 -0700

    modify form for interactivity

--- drew-koch-b12/main.js
+++ drew-koch-b12/main.js
@@ -15,3 +15,14 @@ $(".crazy-skills ul").append('<li>New Line Item!!!!</li>');
 $('img').click(function(){
   alert("Ouch. Stop it :(");
 })
+
+$("#name").on("blur", function() {
+  var name = $(this).val();
+  if (name === "Dale" || name === "DALE" || name === "dale") {
+    $('.contact-us').fadeOut(1000, function() {
+      alert("STOP HARRASING US, DALE");
+    });
+  } else {
+  $('#submit').text('Press me, ' + name + "!");
+  }
+});
